d2eb6e5277e029aa5b95b401f55d8775cc56d40a
siddharthoak <sidoak@gmail.com>
2020-02-21 09:31:15 +0530

    Introduce error

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ var server = http.createServer(function(request, response) {
 var port = 80;
 server.listen(port);
 
-console.log("Server running at http://localhost:%d", port);
+console.log("Server running at http://localhost:%d", port
